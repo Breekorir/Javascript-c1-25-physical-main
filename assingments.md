6482c367271917d6a842a12c5a49d1357a55483b
BreeKorir <violetlamai521@gmail.com>
2025-03-01 21:26:34 +0300

    js

---
+++ assingments.md
@@ -0,0 +1,6 @@
+## Assignments
+
+- Differentiate between a compiler and an interpretor
+- Discus variable naming rules and variable naming conventions in js
+- What is the difference `==`, `===` in js
+- Explore The Truth Table logic
---
+++ operators.js
@@ -0,0 +1,29 @@
+/**
+ *  - Arithmetic: `+`, `-`, `*`, `/`, `%`
+   - Comparison: `==`, `===`, `!=`, `!==`, `>`, `<`, `>=`, `<=`
+   - Logical: `&&`, `||`, `!`
+   - Assignment: `=`, `+=`, `-=`, etc.
+   - Practical: Write a program to check if a number is even or odd
+ */
+
+   console.log( 5+5 );
+   console.log( 5-5 );
+   console.log( 5*5 );
+   console.log( 5/5 );
+   console.log( 24%5 ); // 0
+let user = "alber" // assignment operator
+console.log( 5==5 ); // true -- equality check operator
+console.log( 5==6 ); // false
+
+// ! not or negation
+
+console.log( 55 !== 55 );
+console.log( 55 >= 35 );
+
+// AND &&
+console.log( 32>5 || 5>10 ) //  T && T -- T ---- The Truth Table
+
+// OR ||
+
+
+console.log( 68%2 == 0 ); // true - even
